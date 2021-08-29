3ca24c13ab55a95f12d73820ac5aeec8b09716e5
Takahiro Fukase <fukase0622@yahoo.co.jp>
2021-08-29 11:51:05 +0900

    Define and use function to print text easily.

--- Battle3.java
+++ Battle3.java
@@ -17,12 +17,12 @@ public class Battle3 extends Thread
 
 		boolean correct = true;
 
-		System.out.println("\n使用キャラクターを2体選択");
+		println("\n使用キャラクターを2体選択");
 		
 		do
 		{
-			System.out.println("【キャラクターNo.1】");
-			System.out.print("エビワラー→１　魔法使い→２　原始人→３ フクロウ→４: ");
+			println("【キャラクターNo.1】");
+			print("エビワラー→１　魔法使い→２　原始人→３ フクロウ→４: ");
 			chooseCharacter1 = new java.util.Scanner(System.in).nextInt();
 
 			if (chooseCharacter1 < 1 || chooseCharacter1 > 4)
@@ -33,8 +33,8 @@ public class Battle3 extends Thread
 
 		do
 		{
-			System.out.println("【キャラクターNo.2】");
-			System.out.print("エビワラー→１　魔法使い→２　原始人→３ フクロウ→４: ");
+			println("【キャラクターNo.2】");
+			print("エビワラー→１　魔法使い→２　原始人→３ フクロウ→４: ");
 			chooseCharacter2 = new java.util.Scanner(System.in).nextInt();
 
 			if (chooseCharacter2 < 1 || chooseCharacter2 > 4)
@@ -42,7 +42,7 @@ public class Battle3 extends Thread
 			else if (chooseCharacter1 == chooseCharacter2)
 			{	
 				correct = false;
-				System.out.println("\n同じキャラクターは使用できません。。\n");
+				println("\n同じキャラクターは使用できません。。\n");
 			}
 			else
 				correct = true;
@@ -90,7 +90,7 @@ public class Battle3 extends Thread
 
 		Mario mario = new Mario();
 
-		System.out.println("\n敵のマリオが現れた！！！");
+		println("\n敵のマリオが現れた！！！");
 
 
 		while (!(character1.getHp() <= 0 && character2.getHp() <= 0) || (mario.getHp() <= 0))//勝負が続く限り（＝両者とも生きている限り）以下のループを繰り返す
@@ -104,12 +104,12 @@ public class Battle3 extends Thread
 			if (character1.getHp() > 0)
 			{
 				x = character1.selectAttack();//xには1~3(Ebiwaraは４も)のいずれかが入る
-				System.out.println(" ");
+				println(" ");
 			}
 			if (character2.getHp() > 0)
 			{
 				y = character2.selectAttack();
-				System.out.println(" ");
+				println(" ");
 			}
 
 			try
@@ -122,7 +122,7 @@ public class Battle3 extends Thread
 			if (character1.getHp() > 0)
 			{
 				character1.callAttack(x, mario, character1, character2);
-				System.out.println(" ");
+				println(" ");
 
 				try
 				{
@@ -135,7 +135,7 @@ public class Battle3 extends Thread
 			if (character2.getHp() > 0)
 			{
 				character2.callAttack(y, mario, character1, character2);
-				System.out.println(" ");
+				println(" ");
 
 
 				try
@@ -169,7 +169,7 @@ public class Battle3 extends Thread
 				mario.fireBall(character2);
 			if (r == 4)
 				mario.throwShell(character2);
-			System.out.println(" ");
+			println(" ");
 
 
 			try
@@ -186,19 +186,27 @@ public class Battle3 extends Thread
 		//上のループの仕組みにより、以下２つのif構文のうち、必ず１つだけが実行される。
 
 		if (mario.getHp() == 0)
-			System.out.println(character1.getNamae() + "&" + character2.getNamae() + "のチームの勝ち！");
+			println(character1.getNamae() + "&" + character2.getNamae() + "のチームの勝ち！");
 		if (character1.getHp() == 0 && character2.getHp() == 0)
-			System.out.println("マリオの勝ち！");
+			println("マリオの勝ち！");
 		
+	}
+
+	private static void print(String text) {
+		System.out.print(text);
+	}
+
+	private static void println(String text) {
+		System.out.println(text);
 	}
 
 	private static void showStatus(Mario mario, Character character1, Character character2) {
 
-		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
-		System.out.println("マリオHP：" + mario.getHp() + "\n");
-		System.out.print(character1.getNamae() + "HP：" + character1.getHp() + "  ");
-		System.out.println(character2.getNamae() + "HP：" + character2.getHp());
-		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");	
+		println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		println("マリオHP：" + mario.getHp() + "\n");
+		print(character1.getNamae() + "HP：" + character1.getHp() + "  ");
+		println(character2.getNamae() + "HP：" + character2.getHp());
+		println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");	
 	}
 
 }
